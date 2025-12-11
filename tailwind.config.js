08bad8034a323c618d0ddde581796d33e6a7495a
你的名字 <>
2025-12-11 20:48:50 +0800

    Build: Replace Tailwind CDN with local build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/**/*.{js,ts,jsx,tsx}",
+    "./template.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
